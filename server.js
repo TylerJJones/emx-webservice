298eb40d9446f3ac2c9cbeb9f036e6e5fb6b7f13
Tyler Jones <tylerjones@Tyler-Jones-2.local>
2018-09-22 09:51:05 -0500

    text update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello EMX Digital!');
+  res.send('Hello, EMX Digital! Welcome to my web app ran through Google Cloud Platform.');
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
