8e18134e96a1d1ffc4416129d775e868a4374436
Tyler Jones <tylerjones@Tyler-Jones-2.local>
2018-09-24 16:22:54 -0500

    trim down function

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Enable Express
 const express = require('express');
 const app = express();
 
@@ -7,64 +6,9 @@ const app = express();
 
 // Ping Request
 app.get('/', (req, res) => {
-  res.send('OK');
-  next();
-});
-
-// Can you provide proof of eligibility to work in the US?
-app.get('/?q=Status&d=Can+you+provide+proof+of+eligibility+to+work+in+the+US%3F', (req, res) => {
-  res.send('Yes');
-  next();
-});
-
-// Please provide a URL where we can download the source code of your resume and submssion of web service.
-app.get('/?q=Source', (req, res) => {
-  res.send('https://github.com/TylerJJones/emx-webservice.git');
-});
-
-// What is your Email Address?
-app.get('/?q=Email', (req, res) => {
-  res.send('tylerjjones09@gmail.com');
-});
-
-// Which position are you applying for?
-app.get('/?q=Position', (req, res) => {
-  res.send('Lead or Senior Software Engineer');
-});
-
-// How many years or software development experience do you have?
-app.get('/?q=Years', (req, res) => {
-  res.send('6+ years experience as a software developer, 11+ in web development.');
-});
-
-// Please provide a URL where we can download your resume and cover letter.
-app.get('/?q=Resume', (req, res) => {
-  res.send('https://github.com/TylerJJones/emx-webservice.git');
-});
-
-// Please list your relevant university degree(s).
-app.get('/?q=Degree', (req, res) => {
-  res.send('Associates of Design and Technology from IADT Chicago');
-});
-
-// What is your full name?
-app.get('/?q=Name', (req, res) => {
-  res.send('Tyler Jones');
-});
-
-// How did you hear about this position?
-app.get('/?q=Referrer', (req, res) => {
-  res.send('Indeed');
-});
-
-// Please provide a phone number we can use to reach you.
-app.get('/?q=Phone', (req, res) => {
-  res.send('573-680-5560');
-});
-
-// Please solve this puzzle
-app.get('/?q=Puzzle', (req, res) => {
-  res.send('ABCD\nA=>>>\nB<=>>\nC<<=>\nD<<<=');
+  // res.send('OK');
+  var q = request.query.q;
+  response.end("I have received the Q: " + q);
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
