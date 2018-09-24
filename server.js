15eace53bae9cf625b84e06b11a357080db8d13c
Tyler Jones <tylerjones@Tyler-Jones-2.local>
2018-09-24 15:37:21 -0500

    syntax error fix

--- server.js
+++ server.js
@@ -11,67 +11,62 @@ app.get('/', (req, res) => {
 });
 
 // Ping Request
-app.get('/?q=Ping&d=Please+return+OK+so+that+I+know+your+service+works.', (req, res) => {
+app.get('/?q=Ping', (req, res) => {
   res.send('OK');
 });
 
 // Can you provide proof of eligibility to work in the US?
-app.get('/?q=Status&d=Can+you+provide+proof+of+eligibility+to+work+in+the+US%3', (req, res) => {
+app.get('/?q=Status', (req, res) => {
   res.send('Yes');
 });
 
 // Please provide a URL where we can download the source code of your resume and submssion of web service.
-app.get('/?q=Source&d=Please+provide+a+URL+where+we+can+download+the+source+code+of+your+resume+submission+web+service.', (req, res) => {
+app.get('/?q=Source', (req, res) => {
   res.send('https://github.com/TylerJJones/emx-webservice.git');
 });
 
 // What is your Email Address?
-app.get('/?q=Email+Address&d=What+is+your+email+address%3F', $
+app.get('/?q=Email', (req, res) => {
   res.send('tylerjjones09@gmail.com');
 });
 
 // Which position are you applying for?
-app.get('/?q=Position&d=Which+position+are+you+applying+for%3F', $
+app.get('/?q=Position', (req, res) => {
   res.send('Lead or Senior Software Engineer');
 });
 
 // How many years or software development experience do you have?
-app.get('/?q=Years&d=How+many+years+of+software+development+experience+do+you+have%3F', $
+app.get('/?q=Years', (req, res) => {
   res.send('6+ years experience as a software developer, 11+ in web development.');
 });
 
 // Please provide a URL where we can download your resume and cover letter.
-app.get('/?q=Resume&d=Please+provide+a+URL+where+we+can+download+your+resume+and+cover+letter.', $
+app.get('/?q=Resume', (req, res) => {
   res.send('https://github.com/TylerJJones/emx-webservice.git');
 });
 
 // Please list your relevant university degree(s).
-app.get('/?q=Degree&d=Please+list+your+relevant+university+degree(s).', $
+app.get('/?q=Degree', (req, res) => {
   res.send('Associates of Design and Technology from IADT Chicago');
 });
 
 // What is your full name?
-app.get('/?q=Name&d=What+is+your+full+name%3F', $
+app.get('/?q=Name', (req, res) => {
   res.send('Tyler Jones');
 });
 
 // How did you hear about this position?
-app.get('/?q=Referrer&d=How+did+you+hear+about+this+position%3F', $
+app.get('/?q=Referrer', (req, res) => {
   res.send('Indeed');
 });
 
 // Please provide a phone number we can use to reach you.
-app.get('/?q=Phone&d=Please+provide+a+phone+number+we+can+use+to+reach+you.', $
-  res.send('573-680-5560');
-});
-
-// Please provide a phone number we can use to reach you.
-app.get('/?q=Phone&d=Please+provide+a+phone+number+we+can+use+to+reach+you.', $
+app.get('/?q=Phone', (req, res) => {
   res.send('573-680-5560');
 });
 
 // Please solve this puzzle
-app.get('/?q=Puzzle&d=Please+solve+this+puzzle%3A%0A+ABCD%0AA-%3E--%0AB--%3E-%0AC--%3D-%0AD--%3C-%0A', $
+app.get('/?q=Puzzle', (req, res) => {
   res.send('ABCD\nA=>>>\nB<=>>\nC<<=>\nD<<<=');
 });
 
